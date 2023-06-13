b46ca83509656eace618e07d8be5409208e8d23c
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-06-13 13:09:57 -0500

    feat: added watch groups

---
+++ api/watchGroupData.js
@@ -0,0 +1,49 @@
+import { clientCredentials } from '../utils/client';
+
+const baseUrl = clientCredentials.databaseURL;
+
+const getWatchGroup = () => new Promise((resolve, reject) => {
+  fetch(`${baseUrl}/watchGroup.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch((error) => reject(error));
+});
+
+const createWatchGroup = (watchGroupObj) => new Promise((resolve, reject) => {
+  fetch(`${baseUrl}/watchGroup.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(watchGroupObj),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateWatchGroup = (payload) => new Promise((resolve, reject) => {
+  fetch(`${baseUrl}/watchGroup/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export { getWatchGroup, createWatchGroup, updateWatchGroup };
