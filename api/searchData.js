ab3453755860f4b304fb728dcafd1f41fdb3fb46
Brandon Highland <coffeemonsterstudio@gmail.com>
2023-07-05 17:06:55 -0500

    syntax fixes

--- api/searchData.js
+++ api/searchData.js
@@ -1,13 +1,14 @@
 import { searchCredentials } from '../utils/client';
 
 const { searchUrl } = searchCredentials;
+readToken = searchCredentials.apiReadToken
 
 const apiSearch = (searchQuery) => new Promise((resolve, reject) => {
   fetch(`${searchUrl}/${searchQuery}&include_adult=false&language=en-US&page=1`, {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWMyYTkwZDdjOWEzOGE2NGVhOGI3ZTQwZGU0MGJlNSIsInN1YiI6IjY0NTk4YTIyZmUwNzdhNWNhZWQ5MzIwNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UfUpWq-Y_d25LY08jjejscma-sO07sxrfVFViK_fY3g',
+      Authorization: `Bearer ${readToken}`,
     },
   })
     .then((response) => response.json())
